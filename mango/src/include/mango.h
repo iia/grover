3961c70b439c64f432a25575ac4ae46275d43cfb
Ishraq Ibne Ashraf <ishraq.i.ashraf@gmail.com>
2019-03-05 02:29:39 +0100

    mango: Rework the I2C state machine and some renaming

--- mango/src/include/mango.h
+++ mango/src/include/mango.h
@@ -1,5 +1,7 @@
 #define I2C_ADDRESS 42
 
+#define DEBUG_UART true
+
 #define GROVEPI_PORTS 11
 
 #define RX_BUF_IDX_PRT 0
@@ -10,57 +12,79 @@
 #define RX_BUF_LEN_FID 1
 #define RX_BUF_LEN_DAT 4
 
-#define RX_BUF_LEN (RX_BUF_LEN_PRT + RX_BUF_LEN_FID + RX_BUF_LEN_DAT)
-#define TX_BUF_LEN 6
+#define RX_TX_BUF_LEN 6
 
 #define RST_INT_DIS_ACK (_BV(TWINT))
 #define RST_INT_ENA_ACK ((_BV(TWINT)) | (_BV(TWEA)))
 
-#define ERR_PRT_HND_INV_DAT -1
-#define ERR_PRT_HND_INV_FID -2
+// FID return values.
+#define FID_RET_OK           0
+#define FID_RET_ERR_INV_FID -1
+#define FID_RET_ERR_INV_DAT -2
 
 typedef enum {
-	PORT_0_FID_0 = 0,
-} PORT_0_FID_t;
+	FID_PORT_A0_CH0_ = 0x01,
+	FID_PORT_A0_CH1_ = 0x02
+} FID_PORT_A0_t;
 
 typedef enum {
-	PORT_1_FID_0 = 0,
-} PORT_1_FID_t;
+	FID_PORT_A1_CH0_ = 0x01,
+	FID_PORT_A1_CH1_ = 0x02
+} FID_PORT_A1_t;
 
 typedef enum {
-	PORT_2_FID_0 = 0,
-} PORT_2_FID_t;
+	FID_PORT_A2_CH0_ = 0x01,
+	FID_PORT_A2_CH1_ = 0x02
+} FID_PORT_A2_t;
 
 typedef enum {
-	PORT_3_FID_0 = 0,
-} PORT_3_FID_t;
+	FID_PORT_SERIAL_CH0_ = 0x01,
+	FID_PORT_SERIAL_CH1_ = 0x02
+} FID_PORT_SERIAL_t;
 
 typedef enum {
-	PORT_4_FID_0 = 0,
-} PORT_4_FID_t;
+	FID_PORT_D2_CH0_ = 0x01,
+	FID_PORT_D2_CH1_ = 0x02
+} FID_PORT_D2_t;
 
 typedef enum {
-	PORT_5_FID_0 = 0,
-} PORT_5_FID_t;
+	FID_PORT_D3_CH0_ = 0x01,
+	FID_PORT_D3_CH1_ = 0x02
+} FID_PORT_D3_t;
 
 typedef enum {
-	PORT_6_FID_0 = 0,
-} PORT_6_FID_t;
+	FID_PORT_D4_CH0_ = 0x01,
+	FID_PORT_D4_CH1_ = 0x02
+} FID_PORT_D4_t;
 
 typedef enum {
-	PORT_7_FID_0 = 0,
-} PORT_7_FID_t;
+	FID_PORT_D5_CH0_ = 0x01,
+	FID_PORT_D5_CH1_ = 0x02
+} FID_PORT_D5_t;
 
 typedef enum {
-	PORT_8_FID_0 = 0,
-} PORT_8_FID_t;
+	FID_PORT_D6_CH0_ = 0x01,
+	FID_PORT_D6_CH1_ = 0x02
+} FID_PORT_D6_t;
 
 typedef enum {
-	PORT_9_FID_0 = 0,
-} PORT_9_FID_t;
+	FID_PORT_D7_CH0_SET_DIN     = 0x01,
+	FID_PORT_D7_CH0_SET_DOU     = 0x02,
+	FID_PORT_D7_CH0_SET_DIN_PUP = 0x03,
+	FID_PORT_D7_CH0_GET_DIR     = 0x04,
+	FID_PORT_D7_CH0_GET_VAL     = 0x05,
+	FID_PORT_D7_CH0_GET_PUP     = 0x07,
+	FID_PORT_D7_CH1_SET_DIN     = 0x08,
+	FID_PORT_D7_CH1_SET_DOU     = 0x09,
+	FID_PORT_D7_CH1_SET_DIN_PUP = 0x0A,
+	FID_PORT_D7_CH1_GET_DIR0    = 0x0B,
+	FID_PORT_D7_CH1_GET_VAL     = 0x0C,
+	FID_PORT_D7_CH1_GET_PUP     = 0x0D
+} FID_PORT_D7_t;
 
 typedef enum {
-	PORT_10_FID_0 = 0,
-} PORT_10_FID_t;
+	FID_PORT_D8_CH0_ = 0x01,
+	FID_PORT_D8_CH1_ = 0x02
+} FID_PORT_D8_t;
 
-typedef int8_t (*PORT_HANDLER_t)(uint8_t);
+typedef void (*PORT_HANDLER_t)(uint8_t);
--- mango/src/mango.c
+++ mango/src/mango.c
@@ -12,189 +12,172 @@
 static struct {
 	uint8_t rx_fid;
 	uint8_t rx_port;
-	bool tx_data_valid;
+	bool rx_tx_data_valid;
 	uint8_t rx_tx_buffer_idx;
-	uint8_t rx_buffer[RX_BUF_LEN];
-	uint8_t tx_buffer[TX_BUF_LEN];
+	uint8_t rx_tx_buffer[RX_TX_BUF_LEN];
 	uint8_t rx_data[RX_BUF_LEN_DAT];
 } ctx_port_handler;
 
-static int8_t port_handler_0(uint8_t fid) {
-	PORT_0_FID_t port_fid = (PORT_0_FID_t)fid;
+static void port_handler_A0(uint8_t fid) {
+	FID_PORT_A0_t port_fid = (FID_PORT_A0_t)fid;
 
 	switch (port_fid) {
 		default:
-			return (int8_t)ERR_PRT_HND_INV_FID;
+			break;
 	}
-
-	return 0;
 }
 
-static int8_t port_handler_1(uint8_t fid) {
-	PORT_1_FID_t port_fid = (PORT_1_FID_t)fid;
+static void port_handler_A1(uint8_t fid) {
+	FID_PORT_A1_t port_fid = (FID_PORT_A1_t)fid;
 
 	switch (port_fid) {
 		default:
-			return (int8_t)ERR_PRT_HND_INV_FID;
+			break;
 	}
-
-	return 0;
 }
 
-static int8_t port_handler_2(uint8_t fid) {
-	PORT_2_FID_t port_fid = (PORT_2_FID_t)fid;
+static void port_handler_A2(uint8_t fid) {
+	FID_PORT_A2_t port_fid = (FID_PORT_A2_t)fid;
 
 	switch (port_fid) {
 		default:
-			return (int8_t)ERR_PRT_HND_INV_FID;
+			break;
 	}
-
-	return 0;
 }
 
-static int8_t port_handler_3(uint8_t fid) {
-	PORT_3_FID_t port_fid = (PORT_3_FID_t)fid;
+static void port_handler_serial(uint8_t fid) {
+	FID_PORT_SERIAL_t port_fid = (FID_PORT_SERIAL_t)fid;
 
 	switch (port_fid) {
 		default:
-			return (int8_t)ERR_PRT_HND_INV_FID;
+			break;
 	}
-
-	return 0;
 }
 
-static int8_t port_handler_4(uint8_t fid) {
-	PORT_4_FID_t port_fid = (PORT_4_FID_t)fid;
+static void port_handler_D2(uint8_t fid) {
+	FID_PORT_D2_t port_fid = (FID_PORT_D2_t)fid;
 
 	switch (port_fid) {
 		default:
-			return (int8_t)ERR_PRT_HND_INV_FID;
+			break;
 	}
-
-	return 0;
 }
 
-static int8_t port_handler_5(uint8_t fid) {
-	PORT_5_FID_t port_fid = (PORT_5_FID_t)fid;
+static void port_handler_D3(uint8_t fid) {
+	FID_PORT_D3_t port_fid = (FID_PORT_D3_t)fid;
 
 	switch (port_fid) {
 		default:
-			return (int8_t)ERR_PRT_HND_INV_FID;
+			break;
 	}
-
-	return 0;
 }
 
-static int8_t port_handler_6(uint8_t fid) {
-	PORT_6_FID_t port_fid = (PORT_6_FID_t)fid;
+static void port_handler_D4(uint8_t fid) {
+	FID_PORT_D4_t port_fid = (FID_PORT_D4_t)fid;
 
 	switch (port_fid) {
 		default:
-			return (int8_t)ERR_PRT_HND_INV_FID;
+			break;
 	}
-
-	return 0;
 }
 
-static int8_t port_handler_7(uint8_t fid) {
-	PORT_7_FID_t port_fid = (PORT_7_FID_t)fid;
+static void port_handler_D5(uint8_t fid) {
+	FID_PORT_D5_t port_fid = (FID_PORT_D5_t)fid;
 
 	switch (port_fid) {
 		default:
-			return (int8_t)ERR_PRT_HND_INV_FID;
+			break;
 	}
-
-	return 0;
 }
 
-static int8_t port_handler_8(uint8_t fid) {
-	PORT_8_FID_t port_fid = (PORT_8_FID_t)fid;
+static void port_handler_D6(uint8_t fid) {
+	FID_PORT_D6_t port_fid = (FID_PORT_D6_t)fid;
 
 	switch (port_fid) {
 		default:
-			return (int8_t)ERR_PRT_HND_INV_FID;
+			break;
 	}
-
-	return 0;
 }
 
-static int8_t port_handler_9(uint8_t fid) {
-	PORT_9_FID_t port_fid = (PORT_9_FID_t)fid;
+static void port_handler_D7(uint8_t fid) {
+	FID_PORT_D7_t port_fid = (FID_PORT_D7_t)fid;
 
 	switch (port_fid) {
 		default:
-			return (int8_t)ERR_PRT_HND_INV_FID;
+			break;
 	}
-
-	return 0;
 }
 
-static int8_t port_handler_10(uint8_t fid) {
-	PORT_10_FID_t port_fid = (PORT_10_FID_t)fid;
+static void port_handler_D8(uint8_t fid) {
+	FID_PORT_D8_t port_fid = (FID_PORT_D8_t)fid;
 
 	switch (port_fid) {
 		default:
-			return (int8_t)ERR_PRT_HND_INV_FID;
+			break;
 	}
-
-	return 0;
 }
 
 static void i2c_init(uint8_t i2c_address) {
 	TWAR = (i2c_address << 1); // Disable general call.
-	TWCR = _BV(TWIE) | _BV(TWEA) | _BV(TWINT) | _BV(TWEN);
+	TWCR |= _BV(TWIE) | _BV(TWEA) | _BV(TWINT) | _BV(TWEN);
 }
 
-static void uart_init(uint32_t baud) {
-	// UART TX only (8-N-1).
-	uint8_t ubrr = ((F_CPU / 16) / baud) - 1;
+static void debug_uart_init(uint32_t baud) {
+	#if DEBUG_UART == true
+		// TX only debug UART (8-N-1).
+		uint8_t ubrr = ((F_CPU / 16) / baud) - 1;
 
-	UBRR0H = (uint8_t)(ubrr & 0x0F00);
-	UBRR0L = (uint8_t)(ubrr & 0x00FF);
-	UCSR0B |= _BV(TXEN0);
-	UCSR0C = (3 << UCSZ00);
+		UBRR0H = (uint8_t)(ubrr & 0x0F00);
+		UBRR0L = (uint8_t)(ubrr & 0x00FF);
+		UCSR0B |= _BV(TXEN0);
+		UCSR0C = (3 << UCSZ00);
+	#endif
 }
 
-static void uart_tx_char(char c) {
-	while (!(UCSR0A & _BV(UDRE0))) {
-		;
-	}
+static void debug_uart_tx_char(char c) {
+	#if DEBUG_UART == true
+		while (!(UCSR0A & _BV(UDRE0))) {
+			;
+		}
 
-	UDR0 = c;
+		UDR0 = c;
+	#endif
 }
 
-static void uart_tx_string(char* s) {
-	while (*s != '\0') {
-		uart_tx_char(*s);
+static void debug_uart_tx_string(char* s) {
+	#if DEBUG_UART == true
+		while (*s != '\0') {
+			debug_uart_tx_char(*s);
 
-		++s;
-	}
+			++s;
+		}
+	#endif
 }
 
 static PORT_HANDLER_t port_handler[GROVEPI_PORTS] =
 	{
-		&port_handler_0,
-		&port_handler_1,
-		&port_handler_2,
-		&port_handler_3,
-		&port_handler_4,
-		&port_handler_5,
-		&port_handler_6,
-		&port_handler_7,
-		&port_handler_8,
-		&port_handler_9,
-		&port_handler_10
+		&port_handler_A0,
+		&port_handler_A1,
+		&port_handler_A2,
+		&port_handler_serial,
+		&port_handler_D2,
+		&port_handler_D3,
+		&port_handler_D4,
+		&port_handler_D5,
+		&port_handler_D6,
+		&port_handler_D7,
+		&port_handler_D8
 	};
 
 int main(void) {
 	cli();
 
-	uart_init(9600);
+	debug_uart_init(9600);
 	i2c_init((uint8_t)I2C_ADDRESS);
 
 	memset(&ctx_port_handler, 0, sizeof(ctx_port_handler));
 
-	uart_tx_string("Grover - Mango v1.0\r\n\0");
+	debug_uart_tx_string("Grover - Mango v1.0\r\n\0");
 
 	sei();
 
@@ -209,55 +192,67 @@ ISR(TWI_vect) {
 	switch((TWSR & 0xFC)) {
 		// Slave RX (Write GrovePi).
 
-		// TWSR = 0x60 (RX: ADDR+W, TX: ACK).
+		// TWSR = 0x60 (RX: START+ADDR+W, TX: ACK).
 		case TW_SR_SLA_ACK:
 			ctx_port_handler.rx_tx_buffer_idx = 0;
-			ctx_port_handler.tx_data_valid = false;
+			ctx_port_handler.rx_tx_data_valid = false;
 			TWCR |= RST_INT_ENA_ACK;
 
 			break;
 
 		// TWSR = 0x80 (Got TW_SR_SLA_ACK previously) (RX: DATA, TX: ACK).
 		case TW_SR_DATA_ACK:
-			ctx_port_handler.rx_buffer[ctx_port_handler.rx_tx_buffer_idx] = TWDR;
-
-			ctx_port_handler.rx_tx_buffer_idx++;
-
-			if (ctx_port_handler.rx_tx_buffer_idx >= RX_BUF_LEN) {
-				ctx_port_handler.rx_tx_buffer_idx = 0;
+			if (ctx_port_handler.rx_tx_buffer_idx == RX_TX_BUF_LEN - 1) {
+				ctx_port_handler.rx_tx_buffer[ctx_port_handler.rx_tx_buffer_idx] = TWDR;
+				TWCR |= RST_INT_DIS_ACK;
 			}
-
-			TWCR |= RST_INT_ENA_ACK;
+			else {
+				ctx_port_handler.rx_tx_buffer[ctx_port_handler.rx_tx_buffer_idx] = TWDR;
+				ctx_port_handler.rx_tx_buffer_idx++;
+				TWCR |= RST_INT_ENA_ACK;
+			}
 
 			break;
 
 		// TWSR = 0x88 (Got TW_SR_SLA_ACK previously) (RX: DATA, TX: NACK).
 		case TW_SR_DATA_NACK:
-			TWCR |= RST_INT_ENA_ACK;
+			ctx_port_handler.rx_tx_buffer_idx = 0;
+			TWCR |= RST_INT_DIS_ACK;
 
 			break;
 
 		// TWSR = 0xA0 (RX: Got STOP/RSTART while being addressed as slave, TX: N/A).
 		case TW_SR_STOP:
-			memcpy(
-				&ctx_port_handler.rx_port,
-				&ctx_port_handler.rx_buffer[RX_BUF_IDX_PRT],
-				RX_BUF_LEN_PRT
-			);
+			if (ctx_port_handler.rx_tx_buffer_idx == RX_TX_BUF_LEN - 1) {
+				ctx_port_handler.rx_tx_data_valid = true;
 
-			memcpy(
-				&ctx_port_handler.rx_fid,
-				&ctx_port_handler.rx_buffer[RX_BUF_IDX_FID],
-				RX_BUF_LEN_FID
-			);
+				memcpy(
+					&ctx_port_handler.rx_port,
+					&ctx_port_handler.rx_tx_buffer[RX_BUF_IDX_PRT],
+					RX_BUF_LEN_PRT
+				);
 
-			memcpy(
-				&ctx_port_handler.rx_data,
-				&ctx_port_handler.rx_buffer[RX_BUF_IDX_DAT],
-				RX_BUF_LEN_DAT
-			);
+				memcpy(
+					&ctx_port_handler.rx_fid,
+					&ctx_port_handler.rx_tx_buffer[RX_BUF_IDX_FID],
+					RX_BUF_LEN_FID
+				);
 
-			port_handler[ctx_port_handler.rx_port](ctx_port_handler.rx_fid);
+				memcpy(
+					&ctx_port_handler.rx_data,
+					&ctx_port_handler.rx_tx_buffer[RX_BUF_IDX_DAT],
+					RX_BUF_LEN_DAT
+				);
+
+				port_handler[ctx_port_handler.rx_port](ctx_port_handler.rx_fid);
+			}
+			else {
+				ctx_port_handler.rx_tx_data_valid = false;
+
+				memset(&ctx_port_handler.rx_tx_buffer, 0, RX_TX_BUF_LEN);
+
+				ctx_port_handler.rx_tx_buffer[1] = (int8_t)FID_RET_ERR_INV_DAT;
+			}
 
 			TWCR |= RST_INT_ENA_ACK;
 
@@ -265,17 +260,16 @@ ISR(TWI_vect) {
 
 		// Slave TX (Read GrovePi).
 
-		// TWSR = 0xA8 (RX: ADDR+R, TX: ACK).
+		// TWSR = 0xA8 (RX: START+ADDR+R, TX: ACK).
 		case TW_ST_SLA_ACK:
 			ctx_port_handler.rx_tx_buffer_idx = 0;
 
-			if (!ctx_port_handler.tx_data_valid) {
-				TWDR = 0;
-			}
-			else {
-				TWDR = ctx_port_handler.tx_buffer[ctx_port_handler.rx_tx_buffer_idx];
+			if (!ctx_port_handler.rx_tx_data_valid) {
+				memset(&ctx_port_handler.rx_tx_buffer, 0, RX_TX_BUF_LEN);
+				ctx_port_handler.rx_tx_buffer[1] = (int8_t)FID_RET_ERR_INV_DAT;
 			}
 
+			TWDR = ctx_port_handler.rx_tx_buffer[ctx_port_handler.rx_tx_buffer_idx];
 			ctx_port_handler.rx_tx_buffer_idx++;
 
 			TWCR |= RST_INT_ENA_ACK;
@@ -284,23 +278,13 @@ ISR(TWI_vect) {
 
 		// TWSR = 0xB8 (RX: ACK, TX: TWDR).
 		case TW_ST_DATA_ACK:
-			if (!ctx_port_handler.tx_data_valid) {
-				TWDR = 0;
-			}
-			else {
-				TWDR = ctx_port_handler.tx_buffer[ctx_port_handler.rx_tx_buffer_idx];
-			}
-
-			ctx_port_handler.rx_tx_buffer_idx++;
-
-			if (ctx_port_handler.rx_tx_buffer_idx >= TX_BUF_LEN) {
-				ctx_port_handler.rx_tx_buffer_idx = 0;
-			}
-
-			if (ctx_port_handler.rx_tx_buffer_idx == 0) {
+			if (ctx_port_handler.rx_tx_buffer_idx == RX_TX_BUF_LEN - 1) {
+				TWDR = ctx_port_handler.rx_tx_buffer[ctx_port_handler.rx_tx_buffer_idx];
 				TWCR |= RST_INT_DIS_ACK;
 			}
 			else {
+				TWDR = ctx_port_handler.rx_tx_buffer[ctx_port_handler.rx_tx_buffer_idx];
+				ctx_port_handler.rx_tx_buffer_idx++;
 				TWCR |= RST_INT_ENA_ACK;
 			}
 
@@ -310,13 +294,14 @@ ISR(TWI_vect) {
 		case TW_ST_DATA_NACK:
 		case TW_ST_LAST_DATA:
 			// Reset internal data.
-			ctx_port_handler.tx_data_valid = false;
+			ctx_port_handler.rx_tx_buffer_idx = 0;
+			ctx_port_handler.rx_tx_data_valid = false;
 
 			TWCR |= RST_INT_ENA_ACK;
 
 			break;
 
 		default:
-			TWCR |= RST_INT_ENA_ACK;
+			TWCR |= RST_INT_DIS_ACK;
 	}
 }
