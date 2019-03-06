bc38dd9eac20f2e780451f5c18c85ba3722d9ba4
Ishraq Ibne Ashraf <ishraq.i.ashraf@gmail.com>
2019-03-06 23:27:33 +0100

    mango: Adapt Docker hub changes

--- mango/mango-build-flash.sh
+++ mango/mango-build-flash.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker pull iia86/mango-farmer:v1
+docker pull iia86/mango_farmer:v1
 
-docker run --privileged --rm -it -v $(pwd)/src/:/root/src/ iia86/mango-farmer:v1 ./scripts/mango-build-flash.sh
+docker run --privileged --rm -it -v $(pwd)/src/:/root/src/ iia86/mango_farmer:v1 ./scripts/mango-build-flash.sh
