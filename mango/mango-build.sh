9a59b112c0bfe042a486beef4ebaa3192fe4cae4
Ishraq Ibne Ashraf <ishraq.i.ashraf@gmail.com>
2019-03-17 00:04:53 +0100

    mango: Adapt to repository name change in Dockerhub

--- mango/mango-build.sh
+++ mango/mango-build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker pull iia86/mango_farmer:v1
+docker pull iia86/env_build_mango:v1
 
-docker run --privileged --rm -it -v $(pwd)/src/:/root/src/ iia86/mango_farmer:v1 ./scripts/./mango-build.sh
+docker run --privileged --rm -it -v $(pwd)/src/:/root/src/ iia86/env_build_mango:v1 ./scripts/./mango-build.sh
